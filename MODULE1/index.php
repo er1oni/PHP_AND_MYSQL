a9246d6df58f957e8fb82d5a86dc8bf305a689a5
er1oni <erjongashi_@hotmail.com>
2024-10-10 20:10:29 +0300

    Initial commit

---
+++ MODULE1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>this is my forst php and mysql page</h1>
+
+
+
+<?php
+ echo "Hello world";
+?>
+
+
+
+</body>
+</html>
---
+++ MODULE2/index.php
@@ -0,0 +1,26 @@
+<?php
+  $school = 'Digital School';
+  //echo "I love $school";
+
+
+  //int, float, string, boolean, array, object, resources, null
+
+  // simple arithmetic's +, -, *, /, %
+
+  $x = 120;
+  $y = 50;
+  /*
+  echo $x + $y;
+  echo $x - $y;
+  echo $x * $y;
+  echo $x / $y;
+  */
+
+  //String
+  $a = 'Digital';
+  $b = 'School';
+  $c = $a.$b;
+
+  echo"$c"
+
+?> 
