432881b3a667c4e7e23a8d963b22a5e621437fd7
er1oni <erjongashi_@hotmail.com>
2025-01-16 19:11:26 +0100

    ...

---
+++ MOVIE MANAGMENT SYSTEM/config.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mms";
+
+
+try{
+    $conn = new PDO ("mysql:host=$server;dbname=$dbname", $user, $password);
+}
+
+?>
